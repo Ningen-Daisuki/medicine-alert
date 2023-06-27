7d5293c8402881c35113b203de2aac4fe20612aa
take32457 <take32457@gmail.com>
2023-06-27 12:44:40 +0900

    正しく環境構築を行った

--- backend/medicine_alert/api/apps.py
+++ backend/medicine_alert/api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MedicineapiConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'medicineAPI'
+    name = 'api'
